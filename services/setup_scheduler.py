38518e3ff4231f906f3eee6503cac8526d878ba2
soue27 <man.dozor@gmail.com>
2026-01-25 19:02:48 +0500

    Добавлен вывод недельной статистики в расписание

--- services/setup_scheduler.py
+++ services/setup_scheduler.py
@@ -1,11 +1,12 @@
 from aiogram import Bot
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+from database.db import session
 from services.backup_jobs import backup_and_upload
 from services.mail_checker import check_mail_and_download
 from loguru import logger
 
-
+from services.statistic import get_statistic_for_week
 
 
 async def start_sheduler(bot: Bot):
@@ -20,13 +21,21 @@ async def start_sheduler(bot: Bot):
         minute=00
         )
     logger.info("Start logging")
-    #Резервное копирование базы данных ежедневно в 21.10
+    #Резервное копирование базы данных ежедневно в 06.10
     scheduler.add_job(
         backup_and_upload,
         trigger="cron",
+        args=(session, bot),  # Pass the bot instance to the function
+        day_of_week="mon",
         hour=1,
         minute=10
         )
+    scheduler.add_job(
+        get_statistic_for_week,
+        trigger="cron",
+        hour=5,
+        minute=15
+    )
     scheduler.start()
     pass
 
