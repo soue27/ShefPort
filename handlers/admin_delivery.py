58c4b51f6a04002ab87b2a71db42d58f3de183dc
soue27 <man.dozor@gmail.com>
2026-02-03 10:25:15 +0500

    Скорректировано время

--- handlers/admin_delivery.py
+++ handlers/admin_delivery.py
@@ -50,6 +50,7 @@ async def get_delivery_time(message: Message, state: FSMContext):
     mode.start_at = start  # или строка: f"{start_time:%H:%M}"
     mode.end_at = end # или строка: f"{end_time:%H:%M}"
     mode.is_enabled = True
+    session.execute("SET TIME ZONE 'UTC'")
     session.commit()
     await message.answer(f"Доставка включена в период c {start} до {end} часов")
     await state.clear()
